795b26b868ce81551e6e5fe7a4f2f49013275131
Nishanth Mahasamudram <m.nishanth8@gmail.com>
2023-01-07 11:01:54 +0000

    class 3 JSX functional components component composition

--- App.js
+++ App.js
@@ -1,30 +1,50 @@
-const heading = React.createElement(
-    "h1", 
-    {
-        id: "title",
-        key: "h1"
-    },
-    "Heading 1"
+import { createElement } from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement(
+//     "h1", 
+//     {
+//         id: "title",
+//         key: "h1"
+//     },
+//     "Heading 1"
+// );
+
+// const heading2 = React.createElement(
+//     "h2",
+//     {
+//         id: "title",
+//         key: "h2"
+//     },
+//     "Heading 2"
+// );
+
+// const container = React.createElement(
+//     "div",
+//     {
+//         id: "container"
+//     },
+//     [heading, heading2]
+
+// );
+
+const heading = <h1 id="title" key="h2">Nishanth</h1>;
+
+const Title = () => (
+    <h1 id="title" key="h2">
+        Component Composition - passing components into components
+    </h1>
 );
 
-const heading2 = React.createElement(
-    "h2",
-    {
-        id: "title",
-        key: "h2"
-    },
-    "Heading 2"
+const HeaderComponent = () => (
+    <div>
+        {heading}
+        <Title/> //Component inside a component(Component composition)
+        <h1>Namaste React functional component</h1>
+        <h2>This is a h2 tag</h2>
+    </div>
 );
-
-const container = React.createElement(
-    "div",
-    {
-        id: "container"
-    },
-    [heading, heading2]
-
-)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(<HeaderComponent/>);
